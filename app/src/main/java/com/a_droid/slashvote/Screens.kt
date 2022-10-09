8dd05e50056cae8eaa67f6c41d941b38739cab7c
SuyogMahajan <mahajansuyog9322@gmail.com>
2022-10-09 21:03:10 +0530

    implemented screens route class

---
+++ app/src/main/java/com/a_droid/slashvote/Screens.kt
@@ -0,0 +1,13 @@
+package com.slashvote.slashvote
+
+sealed class Screens(val route:String){
+
+object HomeScreen : Screens("home_screen")
+object QuestionScreen : Screens("question_screen")
+object ChoicesScreen : Screens("choices_screen")
+object StaticsScreen : Screens("statics_screen")
+object OtpScreen : Screens("otp_screen")
+object VotingScreen : Screens("voting_screen")
+object ResultScreen : Screens("result_screen")
+
+}
